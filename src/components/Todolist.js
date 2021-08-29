de6b2f32bd88ca3147dc072c37702484a705062c
VullnetJ <v.jata24@gmail.com>
2021-08-29 18:25:05 +0300

    first commit

---
+++ src/components/Todolist.js
@@ -0,0 +1,68 @@
+
+import React, {useState, useRef} from 'react';
+import { AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+function Todolist() {
+    const [todo, setTodo] = useState({desc: '', date: '', priority:''});
+    const [todos, setTodos] = useState([]);
+    
+ 
+    const inputChanged = (event) => {
+        setTodo({...todo, [event.target.name]: event.target.value});
+      }
+      const addTodo = (event) => {
+        event.preventDefault();
+        setTodos([...todos, todo]);
+      }
+      const columns = [
+          {headerName: 'Description', field: 'desc', sortable: true, filter: true},
+          {headerName: 'Date', field: 'date', sortable: true, filter: true},
+          {headerName: 'Priority', field: 'priority', sortable: true, filter: true,
+            cellStyle: params => params.value ==="High"? {color: 'red'} : {color: 'black'}
+        }
+          
+      ]
+      const gridRef= useRef();
+
+      const deleteTodo = () => {
+          if (gridRef.current.getSelectedNodes().length > 0) 
+               setTodos(todos.filter((todo, index) => index !== gridRef.current.getSelectedNodes()[0].childIndex)) 
+         else 
+             alert('Select row first');
+      } 
+    
+
+      return (
+        <div>
+         
+            <input type="text" onChange={inputChanged} placeholder="Description" name="desc" value={todo.desc}/>
+            <input type="date" onChange={inputChanged} placeholder="Date" name="date" value={todo.date}/>
+            <input type="text" onChange={inputChanged} placeholder="Priority" name="priority" value={todo.priority}/>
+           <button onClick={addTodo}>Add</button>
+            <button onClick={deleteTodo}>Delete</button>
+         
+          <div
+          className="ag-theme-material"
+          style={{
+            height: '700px',
+            width: '80%',
+            margin: 'auto',
+          }}
+          >
+            <AgGridReact
+            ref={gridRef}
+            onGridReady={params => gridRef.current = params.api}
+            rowSelection="single"
+            // rowSelection=""
+            columnDefs={columns}
+            rowData={todos}
+            > 
+          </AgGridReact> 
+          </div>
+          
+        </div>
+      );
+    };
+    export default Todolist;
